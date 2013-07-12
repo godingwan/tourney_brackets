6cbeeee821d16a796ce1d63a544f8741bee41c82
Joshua Lee <jsungholee@gmail.com>
2013-07-12 16:48:44 -0400

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+TourneyBrackets::Application.config.secret_key_base = '8ecce439b0324ddcf341c853a247d39f6d94ccac5f4423548d5c79a2fbf8f506ed11bd142be3d8b60434e3614d4d1d3fe9d2531c4277245c4984e4ce154eb0c7'
